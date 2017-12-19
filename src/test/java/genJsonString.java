5f793ab0d57f25305b6ab5d555a8545d4aa52d1f
Benjamin <Benjamin.Zhou@morningstar.com>
2017-12-19 13:25:41 +0800

    init

--- src/test/java/genJsonString.java
+++ src/test/java/genJsonString.java
@@ -1,9 +1,4 @@
-import com.alibaba.fastjson.JSON;
-import com.company.project.model.User;
-
-import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 /**
  * Created by bzhou on 2017/12/18.
@@ -14,14 +9,14 @@ public class genJsonString {
         
         // 生成调试时需要的Json String
         
-        User user = new User();
-        user.setNickName("Nem");
-        user.setPassword("aaaa");
-        DateFormat dateFormat2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        user.setRegisterDate(dateFormat2.parse("2017-06-23 14:24:23"));
-        user.setSex(1);
-        String a = JSON.toJSONString(user, true);
-        System.out.print(a);
+//        User user = new User();
+//        user.setNickName("Nem");
+//        user.setPassword("aaaa");
+//        DateFormat dateFormat2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        user.setRegisterDate(dateFormat2.parse("2017-06-23 14:24:23"));
+//        user.setSex(1);
+//        String a = JSON.toJSONString(user, true);
+//        System.out.print(a);
 
 
     }
